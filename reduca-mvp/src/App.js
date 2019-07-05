58ad1bdcd6aa1405aa102b26e5b398bf30d1d266
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-07-05 15:34:16 -0300

    configurando webpack para load css

---
+++ reduca-mvp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from './hoc/Layout/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
