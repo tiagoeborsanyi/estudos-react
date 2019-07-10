f880e982d1a50f5c009e7ac564b2ffa443983d1d
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-07-10 17:32:34 -0300

    carroussel feita de forma basica

---
+++ reduca-mvp/src/containers/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Carroussel from '../../components/Carroussel/Carroussel';
+
+class Home extends Component {
+    render () {
+        return (
+            <div>
+                <Carroussel />
+            </div>
+        );
+    }
+}
+
+export default Home;
