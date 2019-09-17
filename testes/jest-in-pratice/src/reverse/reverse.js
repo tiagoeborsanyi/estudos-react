71216494003c0122ff10070d15a9520b9fc69f7d
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-09-17 17:54:46 -0300

    trabalhando function rever testes

---
+++ testes/jest-in-pratice/src/reverse/reverse.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const every = (arr, callback) => {
+    
+}
+
+export default every;
---
+++ testes/jest-in-pratice/src/reverse/reverse.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import { expect } from 'chai';
+import reverse from './reverse';
+
+it('reverse should be a function', () => {
+    expect(reverse).to.be.a('function');
+});
+
+it('reverse([0, 2, 3], (item) => item) should return false', () => {
+    expect(reverse([0, 2, 3], (item) => item)).to.be.deep.equal([3, 2, 0]);
+});
+
+
