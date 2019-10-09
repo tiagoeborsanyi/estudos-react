6dc1b416fcda507457564d116a77f4381116af97
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-09 15:36:31 -0300

    trabalhando na function de paginacao

--- testes/workflow-testes/src/utils/pagination/index.js
+++ testes/workflow-testes/src/utils/pagination/index.js
@@ -1,8 +1,66 @@
 'use strict';
 
+const centerRule = ({total, activePage}) => {
+    if (activePage -1 <= 0) {
+        return 1
+    }
+    return activePage - 1;
+}
+
 const pagination = ({total, activePage}) => {
+    if (total <= 5) {
     // return Array.apply(null, {length: total}).map((_, i) => i + 1);
     return Array.from({ length: total }, (_, i) => i+1);
+    }
+    const visiblePages = 3;
+    let pages = [
+        1,
+        ...Array.from({length: visiblePages}, (_, i) => i+(centerRule({total, activePage}))),
+        total
+    ]
+
+    pages = pages.filter((page, index, array) => array.indexOf(page) === index);
+
+    let firstPage = pages[0];
+    let secondPage = pages[1];
+    console.log(secondPage)
+    if (secondPage === (firstPage+2)) {
+        pages = [
+            firstPage,
+            firstPage+1,
+            ...pages.slice(1)
+        ]
+    }
+
+    // if ((firstPage+1) !== secondPage) {
+    //     pages = [
+    //         firstPage,
+    //         "...",
+    //         ...pages.slice(2)
+    //     ]
+    // }
+
+    let penultimatePage = pages[pages.length - 2];
+    let lastPage = pages[pages.length - 1];
+    if (penultimatePage === (lastPage - 2)) {
+        pages = [
+            ...pages.slice(0, -1),
+            lastPage - 1,
+            lastPage
+        ]
+    }
+
+    penultimatePage = pages[pages.length - 2];
+    lastPage = pages[pages.length - 1];
+    if (penultimatePage <= (lastPage -2)) {
+        pages = [
+            ...pages.slice(0, -1),
+            '...',
+            lastPage
+        ];
+    }
+
+    return pages;
 }
 
 export default pagination;
--- testes/workflow-testes/src/utils/pagination/pagination.test.js
+++ testes/workflow-testes/src/utils/pagination/pagination.test.js
@@ -18,3 +18,39 @@ test('pagination({total: 2, activePage: 1}) should return [1, 2]', () => {
     const result = [1, 2];
     expect(pagination(params)).to.be.deep.equal(result);
 });
+
+test('pagination({total: 5, activePage: 1}) should return [1, 2, 3, 4, 5]', () => {
+    const params = { total: 5, activePage: 1 };
+    const result = [1, 2, 3, 4, 5];
+    expect(pagination(params)).to.be.deep.equal(result);
+});
+
+test('pagination({total: 6, activePage: 1}) should return [1, 2, 3, "...", 6]', () => {
+    const params = { total: 6, activePage: 1 };
+    const result = [1, 2, 3, "...", 6];
+    expect(pagination(params)).to.be.deep.equal(result);
+});
+
+test('pagination({total: 6, activePage: 2}) should return [1, 2, 3, "...", 6]', () => {
+    const params = { total: 6, activePage: 2 };
+    const result = [1, 2, 3, "...", 6];
+    expect(pagination(params)).to.be.deep.equal(result);
+});
+
+test('pagination({total: 6, activePage: 3}) should return [1, 2, 3, 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 3 };
+    const result = [1, 2, 3, 4, 5, 6];
+    expect(pagination(params)).to.be.deep.equal(result);
+});
+
+test('pagination({total: 6, activePage: 4}) should return [1, 2, 3, 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 4 };
+    const result = [1, 2, 3, 4, 5, 6];
+    expect(pagination(params)).to.be.deep.equal(result);
+});
+
+test('pagination({total: 6, activePage: 5}) should return [1, "...", 4, 5, 6]', () => {
+    const params = { total: 6, activePage: 5 };
+    const result = [1, "...", 4, 5, 6];
+    expect(pagination(params)).to.be.deep.equal(result);
+});
