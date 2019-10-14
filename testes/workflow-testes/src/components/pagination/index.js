406feac078dfb61300576f323f24edd7d97c6c5f
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-13 21:04:32 -0300

    paginacao com storybook feito - githubApp

--- testes/workflow-testes/src/components/pagination/index.js
+++ testes/workflow-testes/src/components/pagination/index.js
@@ -1,18 +1,31 @@
 'use strict';
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import pagination from '../../utils/pagination/index';
+import Page from './page';
 
-const Pagination = ({ total, activePage }) => (
+const Pagination = ({ total, activePage, pageLink, onClick }) => (
     <div>
-        <ul>
+        <ul style={{listStyle: 'inside'}}>
             {pagination({total, activePage}).map((page, index) => (
-                <li key={index}>
-                    <a href="#">{page}</a>
+                <li key={index} style={activePage === page ? { color: 'red'} : null}>
+                    <Page page={page} pageLink={pageLink.replace('%page%', page)} onClick={onClick} />
                 </li>
             ))}
         </ul>
     </div>
 )
+
+Pagination.defaultProps = {
+    pageLink: '',
+    activePage: 1
+}
+Pagination.PropTypes = {
+    total: PropTypes.number,
+    activePage: PropTypes.number,
+    pageLink: PropTypes.string,
+    onClick: PropTypes.func
+}
 
 export default Pagination;
--- testes/workflow-testes/src/components/pagination/pagination.stories.js
+++ testes/workflow-testes/src/components/pagination/pagination.stories.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import Pagination from './index';
 
 export default {
@@ -9,10 +9,33 @@ export default {
     title: 'Pagination'
 }
 
-export const withoutProps = () => <Pagination 
-                                        total={10}
-                                        activePage={6} />
+export const withoutProps = () => <Pagination />
 
+export const withTotalAnActivePage = () => <Pagination 
+                                        total={10}
+                                        activePage={5} />
+
+export const withPageLink = () => <Pagination 
+                                        total={3}
+                                        activePage={1}
+                                        pageLink='https://google.com/%page%' />
+
+export const withCallback = () => <Pagination 
+                                        total={15}
+                                        activePage={7}
+                                        pageLink='https://google.com/%page%'
+                                        onClick={(page) => {
+                                            window.alert(page)
+                                        }} />
 withoutProps.story = {
     name: 'without props'
+}
+withTotalAnActivePage.story = {
+    name: 'with toal and activePage'
+}
+withPageLink.story = {
+    name: 'with page link'
+}
+withCallback.story = {
+    name: 'with callback'
 }
