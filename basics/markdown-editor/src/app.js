98a62e6b401903038ec9733445b4e78f55faca79
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-16 14:28:35 -0300

    componente markDown isolado de controled e uncontroled - markdownEditor

--- basics/markdown-editor/src/app.js
+++ basics/markdown-editor/src/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import React, { Component } from 'react'
+import MarkdownEditor from './markdown-editor';
 
 import './css/style.css'
 
@@ -11,26 +12,19 @@ class App extends Component {
       value: ''
     }
 
-    this.handleSubmit = (e) => {
-      e.preventDefault()
-      console.log(e.target.textarea.value)
+    this.handleChange = (e) => {
       this.setState({
-        value: e.target.textarea.value
+        value: e.target.value
       })
     }
   }
 
   render () {
     return (
-      <div className='editor'>
-        <form onSubmit={this.handleSubmit}>
-          <textarea name="textarea" />
-          <button type="submit">Renderizar markup</button>
-        </form>
-        <div className='view'>
-          {this.state.value}
-        </div>
-      </div>
+      <MarkdownEditor 
+        value={this.state.value} 
+        handleChange={this.handleChange}
+      />
     )
   } 
 } 
---
+++ basics/markdown-editor/src/markdown-editor.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, { PropTypes} from 'react';
+
+import './css/style.css'
+
+const MarkdownEditor = ({ value, handleChange }) => (
+    <div className='editor'>
+        <textarea value={value} onChange={handleChange} />
+        <div className='view'>
+            {value}
+        </div>
+    </div>
+)
+
+MarkdownEditor.propTypes = {
+    value: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired
+}
+
+export default MarkdownEditor
