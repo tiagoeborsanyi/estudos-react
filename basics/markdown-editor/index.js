356304eff79120a4d0350200cd1b3dc5c9248416
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-17 14:16:47 -0300

    add server nodejs - markdownEditor

---
+++ basics/markdown-editor/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { join } = require('path')
+const express = require('express')
+const compression = require('compression')
+const app = express()
+
+app.use(compression())
+
+app.use(express.static(join(__dirname, 'dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(5000, () => console.log('listen on 5000'))
