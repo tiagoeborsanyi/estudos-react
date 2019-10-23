38597483ddb9879074ad5783faa38aa420916d4d
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-23 11:04:26 -0300

    add proxy - emaily

--- emaily/server/client/src/App.js
+++ emaily/server/client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/auth/google"
           target="_blank"
           rel="noopener noreferrer"
         >
