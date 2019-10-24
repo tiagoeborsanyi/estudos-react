b5bf480cecbaa65507e112abab46de295ae01e54
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-10-23 23:20:31 -0300

    refatorado e iniciado cliente - emaily

--- emaily/server/services/passport.js
+++ emaily/server/services/passport.js
@@ -19,18 +19,16 @@ passport.use(new GoogleStategy({
     callbackURL: '/auth/google/callback',
     proxy: true
   },
-  (accessToken, refreshToken, profile, done) => {
+  async (accessToken, refreshToken, profile, done) => {
 
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if (existingUser) {
-          // we already have a record with the given profile id
-          console.log(existingUser)
-          done(null, existingUser)
-        } else {
-          // we don's have a user record with this ID, make a new record
-          new User({ googleId: profile.id }).save().then(user => done(null, user));
-        }
-      })
+    const existingUser = await User.findOne({ googleId: profile.id })
+    if (existingUser) {
+      // we already have a record with the given profile id
+      console.log(existingUser)
+      return done(null, existingUser)
+    }
+    // we don's have a user record with this ID, make a new record
+    const user = await new User({ googleId: profile.id }).save();
+    done(null, user);
   }
 ));
