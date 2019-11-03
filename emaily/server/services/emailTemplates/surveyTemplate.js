15327655c783bfdad25999ca6782ec6d330d0c03
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-03 11:23:43 -0300

    change path to surveyTemplate - emaily

--- emaily/server/services/emailTemplates/surveyTemplate.js
+++ emaily/server/services/emailTemplates/surveyTemplate.js
@@ -7,12 +7,13 @@ module.exports = (survey) => {
                 <div style="text-align: center">
                     <h3>I'd like your input!</h3>
                     <p>Please answer the following question:</p>
+                    <h3>${survey.title}</h3>
                     <p>${survey.body}</p>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
                     </div>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
                     </div>
                 </div>
             </body>
