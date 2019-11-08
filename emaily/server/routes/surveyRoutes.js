181a55d54de700130c1e8b68fcdf36a402087f16
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-08 11:11:03 -0300

    refactor code surveys webhooks - emaily

--- emaily/server/routes/surveyRoutes.js
+++ emaily/server/routes/surveyRoutes.js
@@ -17,18 +17,20 @@ module.exports = app => {
     app.post('/api/surveys/webhooks', async (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.map(req.body, ({ email, url }) => {
-            const match = p.test(new URL(url).pathname);
-            // console.log('match', match);
-            if (match) {
-                // console.log(match);
-                return { email, surveyId: match.surveyId, choice: match.choice };
-            }
-        });
+        const events = _.chain(req.body)
+                        .map(({ email, url }) => {
+                            const match = p.test(new URL(url).pathname);
+                            // console.log('match', match);
+                            if (match) {
+                                // console.log(match);
+                                return { email, surveyId: match.surveyId, choice: match.choice };
+                            }
+                        })
+                        .compact()
+                        .uniqBy('email', 'surveyId')
+                        .value();
 
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-        console.log(uniqueEvents);
+        console.log(events);
 
         res.send({});
     });
