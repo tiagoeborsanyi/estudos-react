4159d01c39533eb44a3f021585a8b3fc56196a42
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-08 23:45:58 -0300

    final code surveyRoute webook - emaily

--- emaily/server/routes/surveyRoutes.js
+++ emaily/server/routes/surveyRoutes.js
@@ -10,27 +10,38 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
     });
 
     app.post('/api/surveys/webhooks', async (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.chain(req.body)
-                        .map(({ email, url }) => {
-                            const match = p.test(new URL(url).pathname);
-                            // console.log('match', match);
-                            if (match) {
-                                // console.log(match);
-                                return { email, surveyId: match.surveyId, choice: match.choice };
-                            }
-                        })
-                        .compact()
-                        .uniqBy('email', 'surveyId')
-                        .value();
-
-        console.log(events);
+        _.chain(req.body)
+        .map(({ email, url }) => {
+            const match = p.test(new URL(url).pathname);
+            // console.log('match', match);
+            if (match) {
+                // console.log(match);
+                return { email, surveyId: match.surveyId, choice: match.choice };
+            }
+        })
+        .compact()
+        .uniqBy('email', 'surveyId')
+        .each(({ surveyId, email, choice }) => {
+            Survey.updateOne({
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email, responded: false }
+                }
+            },
+            {
+                $inc: { [choice]: 1 },
+                $set: { 'recipients.$.responded': true },
+                lestResponded: new Date()
+            }).exec();
+        })
+        .value();
 
         res.send({});
     });
@@ -59,6 +70,6 @@ module.exports = app => {
             res.status(422).send(e);
         }
 
-        res.send(req.user);
+        // res.send(req.user);
     })
 };
