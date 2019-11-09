3ff75b361fb227fe9f3094137936add7afc54952
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-11-09 15:09:53 -0300

    cretate simple component surveyList connect actions and reducer - emaily

--- emaily/server/client/src/components/Dashboard.js
+++ emaily/server/client/src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
     return (
         <div>
             Dashboard
             <div className="fixed-action-btn">
+                <SurveyList />
                 <Link to="/surveys/new" className="btn-floating btn-large red">
                     <i className="material-icons">add</i>
                 </Link>
