0f4b8d15dcb899e50cc00156db724aeb7c519c02
tiago emerick <tiagoeborsanyi@gmail.com>
2019-12-29 16:39:22 -0300

    finish example project - reactFunctions

--- react-functions/src/App.js
+++ react-functions/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+const  App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    {id: 'cg1', text: 'Finish course'},
+    {id: 'cg2', text: 'middle course'},
+    {id: 'cg3', text: 'initial course'}
+  ]);
+
+  const addNewGoalHandler = (newGoal) => {
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => {
+      return prevCourseGoals.concat(newGoal);
+    })
+  }
+
   return (
-    <div className="App">
-      <h2>initial project</h2>
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
